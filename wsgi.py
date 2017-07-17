3cd0d1c2b78c1bbd3d84e1defc7fa06cba09853c
AntonHellbe <antonhellbe2@gmail.com>
2017-07-17 06:37:55 +0000

    Gått över till Gunicorn + nginx för att få reverse proxy funktion

---
+++ wsgi.py
@@ -0,0 +1,5 @@
+from FlaskApp import app as application
+application.secret_key = 'sajdsaddsdjadhaskjdsahdksahdjkadsada21321u787as8d9a'
+
+from FlaskApp import create_app
+application = create_app('production')
